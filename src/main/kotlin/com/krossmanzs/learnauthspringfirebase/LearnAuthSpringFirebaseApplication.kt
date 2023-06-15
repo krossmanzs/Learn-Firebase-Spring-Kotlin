0ff71135fc1836a229f500b72234639a26647297
Krossmanzs <radi.adi78@gmail.com>
2023-06-15 17:41:37 +0700

    First init

---
+++ src/main/kotlin/com/krossmanzs/learnauthspringfirebase/LearnAuthSpringFirebaseApplication.kt
@@ -0,0 +1,11 @@
+package com.krossmanzs.learnauthspringfirebase
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LearnAuthSpringFirebaseApplication
+
+fun main(args: Array<String>) {
+	runApplication<LearnAuthSpringFirebaseApplication>(*args)
+}
---
+++ src/test/kotlin/com/krossmanzs/learnauthspringfirebase/LearnAuthSpringFirebaseApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.krossmanzs.learnauthspringfirebase
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LearnAuthSpringFirebaseApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
